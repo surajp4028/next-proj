2e3c10dd53e2e0c55db23c5b4ed9ccd12b970383
surajp4028 <surajp4028@gmail.com>
2024-06-17 16:08:59 +0530

    second commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 
+    eslint : {
+        
+        ignoreDuringBuilds: true,
+    },
     images :{
         domains:['flowbite.s3.amazonaws.com']
     },
